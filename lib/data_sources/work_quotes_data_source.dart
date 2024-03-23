0d673db21ea4313d31d466707dc9bc533835e9d6
michaell931 <michaell931@gmail.com>
2024-03-23 19:27:05 +0100

    Create Work Quotes Data Source with quotes about work and career

---
+++ lib/data_sources/work_quotes_data_source.dart
@@ -0,0 +1,61 @@
+class WorkQuotesDataSource {
+  Future<List<Map<String, dynamic>>?> getWorkQuotes() async {
+    return [
+      {
+        'id': 1,
+        'content':
+            "Choose a job you love, and you will never have to work a day in your life. (Confucius)",
+      },
+      {
+        'id': 2,
+        'content':
+            "It’s not what you achieve, it’s what you overcome. That’s what defines your career. (Carlton Fisk)",
+      },
+      {
+        'id': 3,
+        'content':
+            "Find out what you like doing best and get someone to pay you for doing it. (Katherine Whitehorn)",
+      },
+      {
+        'id': 4,
+        'content':
+            "I’ve missed more than 9,000 shots in my career. I’ve lost almost 300 games. 26 times, I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed. (Michael Jordan)",
+      },
+      {
+        'id': 5,
+        'content':
+            "Failure doesn’t mean you are a failure it just means you haven’t succeeded yet. (Robert H. Schuller)",
+      },
+      {
+        'id': 6,
+        'content':
+            "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. (Thomas A. Edison)",
+      },
+      {
+        'id': 7,
+        'content':
+            "I think everyone should experience defeat at least once during their career. You learn a lot from it. (Lou Holtz)",
+      },
+      {
+        'id': 8,
+        'content':
+            "I want to look back on my career and be proud of the work, and be proud that I tried everything. (Jon Stewart)",
+      },
+      {
+        'id': 9,
+        'content':
+            "Desire! That’s the one secret of every man’s career. Not education. Not being born with hidden talents. Desire. (Johnny Carson)",
+      },
+      {
+        'id': 10,
+        'content':
+            "Do not be too timid and squeamish about your actions. All life is an experiment. (Ralph Waldo Emerson)",
+      },
+      {
+        'id': 11,
+        'content':
+            "All you need in this life is ignorance and confidence, and then success is sure. (Mark Twain)",
+      },
+    ];
+  }
+}
